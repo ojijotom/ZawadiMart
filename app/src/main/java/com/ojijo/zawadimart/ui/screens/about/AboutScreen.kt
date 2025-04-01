31de90685b8f3b857bfd485e8774d6ad3b23a45c
ojijotom <ojijotom4@gmail.com>
2025-04-01 15:50:58 +0300

    ojijo

---
+++ app/src/main/java/com/ojijo/zawadimart/ui/screens/about/AboutScreen.kt
@@ -0,0 +1,14 @@
+package com.ojijo.zawadimart.ui.screens.about
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AboutScreen(){
+
+}
+@Preview(showBackground = true)
+@Composable
+fun AboutScreenPreview(){
+
+}
